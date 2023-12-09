98e10db148efd5cf3bbcced65b3f0d70932d1998
Kevin Svitač <kevin@svit.ac>
2023-12-09 21:49:04 +0100

    Slideshow opravy

--- js/app.js
+++ js/app.js
@@ -28,16 +28,16 @@ const SABLONY = {
 </header>
 `,
   footer: `
-<footer id="footer" class="container-fluid pt-4 pb-2 text-center">
+<footer id="footer" class="container-fluid pt-4 pb-2">
   <div
-    class="container col-md-6 col-sm-8 col-10 d-flex justify-content-evenly my-4"
+    class="container d-flex justify-content-evenly my-4"
   >
     <div class="d-flex flex-column">
-      <h2 class="fs-6">Zoznam stránok:</h2>
+      <h2 class="fs-6 fw-bold">Zoznam stránok:</h2>
       <ul id="odkazyFooter" class="list-unstyled"></ul>
     </div>
     <div class="d-flex flex-column">
-      <h2 class="fs-6">Zdroje obrázkov:</h2>
+      <h2 class="fs-6 fw-bold">Zdroje obrázkov:</h2>
       <a
         href="https://openai.com/dall-e-3"
         class="text-decoration-none"
@@ -52,12 +52,11 @@ const SABLONY = {
       >
     </div>
 
-    <p class="align-self-center">
-      <b>Vytvoril:</b> Kevin Svitač
-      <br />
+    <p class="align-self-center text-center">
+      <br/>
 
       <!-- kreatívny bod -->
-      <i>&copy; <span id="aktualnyRok"></span></i>
+      <i class="text-center">&copy; <b>Kevin Svitač</b> <span id="aktualnyRok"></span>. Všetky práva vyhradené.</i>
     </p>
   </div>
 </footer>
